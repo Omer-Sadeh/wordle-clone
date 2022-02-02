b9f083df2e52d5c73e70cceaa80d4f0c392040af
Omer Sadeh <omer.sadeh@protonmail.com>
2022-02-02 23:18:01 +0200

    better structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,10 @@ import Keyboard from './components/Keyboard/Keyboard';
 function App() {
 
   const THEWORD = "BOATS"
-  const [Tries, setTries] = useState(
-    {
-      try1: Array(5).fill([null, "empty"]),
-      try2: Array(5).fill([null, "empty"]),
-      try3: Array(5).fill([null, "empty"]),
-      try4: Array(5).fill([null, "empty"]),
-      try5: Array(5).fill([null, "empty"]),
-      try6: Array(5).fill([null, "empty"])
-    }
-  );
-  const [Attempt, setAttempt] = useState(1);
+  const [Attempt, setAttempt] = useState(0);
   const [LetterNum, setLetterNum] = useState(0);
+  const [BoardTiles, setBoardTiles] = useState(Array(6*5).fill({name: null, state: "empty"}));
+  const [GameEnded, setGameEnded] = useState(false);
 
   const KeyboardTiles = {
     Row1: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
@@ -27,51 +19,50 @@ function App() {
   };
 
   const keyboarPress = (value:String) => {
-    switch(value) {
-      case "DEL": if (LetterNum > 0 && Attempt < 7) updateBoard(null, false);
-        break;
-      case "ENTER": checkAttempt();
-        break;
-      default: if(LetterNum < 5 && Attempt < 7) updateBoard(value, true);
-        break;
+    if(!GameEnded) {
+      switch(value) {
+        case "DEL": if (LetterNum > 0 && Attempt < 6) updateBoard(null, false);
+          break;
+        case "ENTER": checkAttempt();
+          break;
+        default: if(LetterNum < 5 && Attempt < 6) updateBoard(value, true);
+          break;
+      }
     }
   }
 
   const updateBoard = (value:any, next:boolean) => {
-    var newTries = {...Tries};
+    var newBoard = BoardTiles.concat();
     var index = LetterNum;
     if(!next) index--;
-    switch(Attempt) {
-      case 1: newTries.try1[index] = [value, "empty"];
-        break;
-      case 2: newTries.try2[index] = [value, "empty"];
-        break;
-      case 3: newTries.try3[index] =[value, "empty"];
-        break;
-      case 4: newTries.try4[index] = [value, "empty"];
-        break;
-      case 5: newTries.try5[index] = [value, "empty"];
-        break;
-      case 6: newTries.try6[index] = [value, "empty"];
-        break;
-    }
+    newBoard[Attempt*5 + index] = {name: value, state:"empty"};
     if(next) index++;
     setLetterNum(index);
-    setTries(newTries);
+    setBoardTiles(newBoard);
   }
 
   const checkAttempt = () => {
-    var newTries = {...Tries};
-    var currentWord = Tries.try1;
-    if(Attempt < 7 && LetterNum === 5) {
+    var newBoard = BoardTiles.concat();
+    var winCondition = true;
+    if(Attempt < 6 && LetterNum === 5) {
       for (var i = 0; i < 5; i++) {
-        if (currentWord[i][0] === THEWORD[i]) currentWord[i][1] = "correct";
-        else currentWord[i][1] = "wrong";
+        if (newBoard[Attempt*5 + i].name === THEWORD[i]) newBoard[Attempt*5 + i].state = "correct";
+        else newBoard[Attempt*5 + i].state = "wrong";
       }
-      newTries.try1 = currentWord;
-      setTries(newTries);
+      setBoardTiles(newBoard);
       setAttempt(Attempt + 1);
       setLetterNum(0);
+    }
+    for (var i = 0; i < 5; i++) {
+      if (newBoard[Attempt*5 + i].state !== "correct") winCondition = false;
+    }
+    if (winCondition){
+      alert("WIN!");
+      setGameEnded(true);
+    }
+    else if (Attempt === 5){
+      alert("LOSE!");
+      setGameEnded(true);
     }
   }
 
@@ -79,7 +70,7 @@ function App() {
     <div className="App">
       <div className="game-wrapper">
         <Header />
-        <Board words={Tries} />
+        <Board words={BoardTiles} />
         <Keyboard row1={KeyboardTiles.Row1} row2={KeyboardTiles.Row2} row3={KeyboardTiles.Row3} press={keyboarPress}/>
       </div>
     </div>
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import BoardTile from './BoardTile';
 
-function Board({words}:{words:any}) {
+function Board({words}:{words:any[]}) {
     return(
         <div className="board-wrapper">
             <div className='board-limits'>
-                <div className='row'>{words.try1.map((val:any) => <BoardTile value={val[0]} state={val[1]}/>)}</div>
-                <div className='row'>{words.try2.map((val:any) => <BoardTile value={val[0]} state={val[1]}/>)}</div>
-                <div className='row'>{words.try3.map((val:any) => <BoardTile value={val[0]} state={val[1]}/>)}</div>
-                <div className='row'>{words.try4.map((val:any) => <BoardTile value={val[0]} state={val[1]}/>)}</div>
-                <div className='row'>{words.try5.map((val:any) => <BoardTile value={val[0]} state={val[1]}/>)}</div>
-                <div className='row'>{words.try6.map((val:any) => <BoardTile value={val[0]} state={val[1]}/>)}</div>
+                <div className='row'>{words.slice(0,5).map((val:any) => <BoardTile value={val.name} state={val.state}/>)}</div>
+                <div className='row'>{words.slice(5,10).map((val:any) => <BoardTile value={val.name} state={val.state}/>)}</div>
+                <div className='row'>{words.slice(10,15).map((val:any) => <BoardTile value={val.name} state={val.state}/>)}</div>
+                <div className='row'>{words.slice(15,20).map((val:any) => <BoardTile value={val.name} state={val.state}/>)}</div>
+                <div className='row'>{words.slice(20,25).map((val:any) => <BoardTile value={val.name} state={val.state}/>)}</div>
+                <div className='row'>{words.slice(25,30).map((val:any) => <BoardTile value={val.name} state={val.state}/>)}</div>
             </div>
         </div>
     );
