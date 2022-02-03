4161940b54b1c05312502aa72e054652b95a1a6c
Omer Sadeh <omer.sadeh@protonmail.com>
2022-02-03 15:48:07 +0200

    button styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BsFillFileBarGraphFill } from 'react-icons/bs';
+import { FiHelpCircle } from 'react-icons/fi';
+import { MdSettings } from 'react-icons/md';
 
 function Header() {
     return(
         <div className="header-wrapper">
-          <div>button</div>
+          <a className="header-btn header-btn-wrapper"><FiHelpCircle /></a>
           <div className="title">WORDLE</div>
-          <div>button</div>
+          <div className="header-btn-wrapper">
+            <a className="header-btn"><BsFillFileBarGraphFill /></a>{" "}
+            <a className="header-btn"><MdSettings /></a>
+          </div>
+          
         </div>
     );
 }
