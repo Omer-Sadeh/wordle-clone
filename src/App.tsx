c52e6f11c8df2f2e7f361c91f024daa7078d7f4d
Omer Sadeh <omer.sadeh@protonmail.com>
2022-02-03 10:44:25 +0200

    random word generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Game from './components/Game';
 
 function App() {
 
+    var file = require(process.env.PUBLIC_URL + "./files/EnglishWords.txt");
+    const [RandomWord, setRandomWord] = useState("");
+
+    useEffect(() => {
+      fetch(file).then(r => r.text()).then(text => {
+        var words = text.split("\n");
+        var word = words[Math.floor(Math.random() * words.length)].toUpperCase();
+        setRandomWord(word);
+      });
+    }, []);
+
   return (
-    <Game TheWord="BOATS" />
+    <Game TheWord={RandomWord} />
   );
 }
 
